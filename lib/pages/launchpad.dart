58ed5542872a00ba6a72c73337fba625719dcad0
Fachri Hawari <fachri.hawari@gmail.com>
2022-04-26 22:28:36 +0700

    chore: Add few page

--- lib/pages/launchpad.dart
+++ lib/pages/launchpad.dart
@@ -3,16 +3,44 @@ import 'package:flutter/material.dart';
 class Page {
   final String name;
   final String page;
+  final bool isAvailable;
 
-  const Page({required this.name, required this.page});
+  const Page({
+    required this.name,
+    required this.page,
+    required this.isAvailable,
+  });
 }
 
 class Launchpad extends StatelessWidget {
   const Launchpad({Key? key}) : super(key: key);
 
   final List<Page> pages = const [
-    Page(name: "Simple Calculator", page: "simple_calculator"),
+    Page(name: "Todo", page: "todo", isAvailable: false),
+    Page(name: "Calculator", page: "calculator", isAvailable: false),
+    Page(name: "Tic Tac Toe", page: "tictactoe", isAvailable: false),
   ];
+
+  void _handleItemTap(BuildContext context, Page item) {
+    if (item.isAvailable) {
+    } else {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: Text(item.name),
+          content: const Text("Coming soon!"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,27 +50,12 @@ class Launchpad extends StatelessWidget {
       ),
       body: ListView.separated(
         itemCount: pages.length,
-        separatorBuilder: (context, index) => const Divider(),
+        separatorBuilder: (context, index) => const Divider(height: 0),
         itemBuilder: (context, index) {
           final item = pages.elementAt(index);
 
           return ListTile(
-            onTap: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) => AlertDialog(
-                  title: Text(item.name),
-                  actions: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: const Text('Close'),
-                    ),
-                  ],
-                ),
-              );
-            },
+            onTap: () => _handleItemTap(context, item),
             title: Text(item.name),
           );
         },
